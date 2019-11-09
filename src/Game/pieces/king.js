a75b3c8b3e08d6e3096030a27a15e401d338e65a
Kuba <ja.tanski@gmail.com>
2019-11-09 19:24:58 +0100

    fix bug in king. Now king's working.

--- src/Game/pieces/king.js
+++ src/Game/pieces/king.js
@@ -13,19 +13,17 @@ class King extends Piece {
     // DIY
   }
 
-
   // Metoda pomocnicza do znalezienie wszystkich możliwych ruchów, które nie są przefiltrowane przez wszystkie ograniczenia.
 
   findAllMoves(x, y, moves_tab) {
-    for (i = x - 1; i <= x + 1; i++) {
-      for (j = y - 1; j <= y + 1; j++) {
+    for (let i = x - 1; i <= x + 1; i++) {
+      for (let j = y - 1; j <= y + 1; j++) {
         if (!(i === x && j === y)) {
           moves_tab.push([i, j]);
         }
       }
     }
   }
-
 
   // Główna metoda, w której trzeba zapisać wszystkie możliwe ruchy danej bierki
   findLegalMoves(board) {
@@ -34,15 +32,20 @@ class King extends Piece {
 
     const x = this._x; // row
     const y = this._y; // column
-    const v = this._vector // up/down
+    const v = this._vector; // up/down
     const all8moves = [];
-    findAllMoves(x, y, all8moves);
-    let legalMoves = all8moves.filter(tab => { let res; (tab[0] >= 0 && tab[0] <= 7 && tab[1] >= 0 && tab[1] <= 7) ? resp = true : resp = false; return resp });
+    this.findAllMoves(x, y, all8moves);
+    let legalMoves = all8moves.filter(tab => {
+      let resp;
+      tab[0] >= 0 && tab[0] <= 7 && tab[1] >= 0 && tab[1] <= 7
+        ? (resp = true)
+        : (resp = false);
+      return resp;
+    });
     return legalMoves;
     // legalMoves = this.filterOutBoardMoves(allMoves);
     // return legalMoves;
   }
-
 }
 
 export default King;
