5906d06761e80d771f6fe8ddb23585e99bf56512
Zofia Natanek <zofia.natanek@gmail.com>
2019-11-14 21:45:12 +0100

    Extract filtering moves to separate method

--- src/Game/pieces/bishop.js
+++ src/Game/pieces/bishop.js
@@ -8,35 +8,40 @@ class Bishop extends Piece {
     this.vector = this._side == "white" ? -1 : 1; // 1 to góra -1 to dół
   }
 
-  // Metoda znajdująca wszystkie ruchy bez filtra
-  findAllMoves(x, y) {
-    let upLeft = [];
-    let upRight = [];
-    let downLeft = [];
-    let downRight = [];
-    let allMoves = [];
-    for (let i = 1; i < 8; i++) {
-      upLeft.push([x - i, y - i]);
-      upRight.push([x + i, y - i]);
-      downLeft.push([x - i, y + i]);
-      downRight.push([x + i, y + i]);
-    }
-    return (allMoves = upLeft.concat(upRight, downLeft, downRight));
-  }
-
-  findLegalMoves(board) {
-    console.log(board);
-
-    const x = this._x;
-    const y = this._y;
-    let legalMoves = this.findAllMoves(x, y).filter(elem => {
+  // Metoda filtrująca ruchy wychodzące poza szachownicę 
+  filterMoves(arr) {
+    let filteredMoves = arr.filter(elem => {
       elem[0] >= 0 && elem[0] <= 7 && elem[1] >= 0 && elem[1] <= 7
         ? (elem = true)
-        : (elem = false);
-      return elem;
-    });
-    return legalMoves;
+        :(elem = false);
+    return elem;
+  });
+}
+
+// Metoda znajdująca wszystkie ruchy bez filtra
+findAllMoves(x, y) {
+  let upLeft = [];
+  let upRight = [];
+  let downLeft = [];
+  let downRight = [];
+  let allMoves = [];
+  for (let i = 1; i < 8; i++) {
+    upLeft.push([x - i, y - i]);
+    upRight.push([x + i, y - i]);
+    downLeft.push([x - i, y + i]);
+    downRight.push([x + i, y + i]);
   }
+  return (allMoves = upLeft.concat(upRight, downLeft, downRight));
+}
+
+findLegalMoves(board) {
+  console.log(board);
+
+  const x = this._x;
+  const y = this._y;
+  let legalMoves = this.findAllMoves(x, y)
+  return legalMoves;
+}
 }
 
 export default Bishop;
