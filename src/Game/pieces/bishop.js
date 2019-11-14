93240aabd749197da788126e3bd8f392102b30fc
Zofia Natanek <zofia.natanek@gmail.com>
2019-11-14 22:14:00 +0100

    Add limit to the Bishop possible moves so it stops on the fileds with collisions

--- src/Game/pieces/bishop.js
+++ src/Game/pieces/bishop.js
@@ -13,49 +13,70 @@ class Bishop extends Piece {
     let filteredMoves = arr.filter(elem => {
       elem[0] >= 0 && elem[0] <= 7 && elem[1] >= 0 && elem[1] <= 7
         ? (elem = true)
-        :(elem = false);
-    return elem;
-  });
-  return filteredMoves
-}
-
-// Metoda znajdująca wszystkie ruchy i zwracająca przefiltrowaną tablicę zawierającą 4 tablice z ruchami w możliwych kierunkach
-findAllMoves(x, y) {
-
-  const x = this._x;
-  const y = this._y;
-
-  let upLeft = [];
-  let upRight = [];
-  let downLeft = [];
-  let downRight = [];
-  let allMoves = [];
-  for (let i = 1; i < 8; i++) {
-    upLeft.push([x - i, y - i]);
-    downLeft.push([x + i, y - i]);
-    upRight.push([x - i, y + i]);
-    downRight.push([x + i, y + i]);
+        : (elem = false);
+      return elem;
+    });
+    return filteredMoves
   }
-  allMoves.push(this.filterMoves(upLeft), this.filterMoves(downLleft), this.filterMoves(upRight), this.filterMoves(downRight));
-  return allMoves
-}
 
-// Metoda sprawdzająca czy na polach, po których porusza się goniec stoją inne figury
-checkTakenFields(arr, board) {
-  let fields = [];
-  for (let i = 0; i < arr.length; i++){
-    let y = arr[i][0];
-    let x = arr[i][1];
-    if (board[y][x] === undefined ? fields.push('empty') : fields.push('taken'));
+  // Metoda znajdująca wszystkie ruchy i zwracająca przefiltrowaną tablicę zawierającą 4 tablice z ruchami w możliwych kierunkach
+  findAllMoves(x, y) {
+
+    const x = this._x;
+    const y = this._y;
+
+    let upLeft = [];
+    let upRight = [];
+    let downLeft = [];
+    let downRight = [];
+    let allMoves = [];
+    for (let i = 1; i < 8; i++) {
+      upLeft.push([x - i, y - i]);
+      downLeft.push([x + i, y - i]);
+      upRight.push([x - i, y + i]);
+      downRight.push([x + i, y + i]);
+    }
+    allMoves.push(this.filterMoves(upLeft), this.filterMoves(downLleft), this.filterMoves(upRight), this.filterMoves(downRight));
+    return allMoves
   }
-  return fields
-}
 
-findLegalMoves(board) {
+  // Metoda sprawdzająca czy na polach, po których porusza się goniec stoją inne figury
+  checkTakenFields(arr, board) {
+    let fields = [];
+    for (let i = 0; i < arr.length; i++) {
+      let y = arr[i][0];
+      let x = arr[i][1];
+      if (board[y][x] === undefined ? fields.push('empty') : fields.push('taken'));
+    }
+    return fields
+  }
 
-  let legalMoves = this.findAllMoves(x, y)
-  return legalMoves;
-}
+  // Metoda sprawdzająca czy zachodzą kolizje z innymi figurami na torze ruchu gońca i zwracająca indeks pola, na którym dochodzi do kolizji
+
+  findCollisions(arr) {
+    let collisionIndex = arr.findIndex(elem => {
+      return elem == 'taken';
+    })
+    return collisionIndex
+  }
+
+  // Metoda ograniczająca tor ruchu gońca do pól, na których dochodzi do kolizji z innymi figurami (włącznie z tymi polami) i zwracająca tablice z ograniczonym torem ruchu
+
+  delimitPathMoves(arr, index){
+    if(index >= 0) {
+      let delimitedMoves = arr.slice(0, [index + 1]);
+      return delimitedMoves
+    }
+    else {
+      return arr
+    }
+  }
+
+  findLegalMoves(board) {
+
+    let legalMoves = this.findAllMoves(x, y)
+    return legalMoves;
+  }
 }
 
 export default Bishop;
