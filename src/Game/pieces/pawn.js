00c0fface5f200d2e13f3d847dab900a14f5bc13
TomaszLakota <48764424+TomaszLakota@users.noreply.github.com>
2019-11-15 18:29:16 +0100

    tmp

--- src/Game/pieces/pawn.js
+++ src/Game/pieces/pawn.js
@@ -1,42 +1,44 @@
 import Piece from "./piece";
 
 class Pawn extends Piece {
-  constructor(x, y, side) {
-    super(x, y, side);
-    this.name = "pawn";
-    this.display = `<i data-color=${side} class="fas fa-chess-pawn ${side}"></i>`;
-    this._vector = this._side == "white" ? -1 : 1; // 1 to góra -1 to dół
-  }
+   constructor(x, y, side) {
+      super(x, y, side);
+      this.name = "pawn";
+      this.display = `<i data-color=${side} class="fas fa-chess-pawn ${side}"></i>`;
+      this._vector = this._side == "white" ? -1 : 1; // 1 to góra -1 to dół
+   }
 
-  findLegalMoves(board) {
-    const moves = Array();
+   findLegalMoves(board) {
+      const moves = Array();
 
-    if (!board[this._x + this._vector][this._y]) {
-      moves.push([this._x + this._vector, this._y]);
+      if (!board[this._x + this._vector][this._y]) {
+         moves.push([this._x + this._vector, this._y]);
 
-      if (this._pristine && !board[this._x + this._vector * 2][this._y]) {
-        moves.push([this._x + this._vector * 2, this._y]);
+         if (this._pristine && !board[this._x + this._vector * 2][this._y]) {
+            moves.push([this._x + this._vector * 2, this._y]);
+         }
       }
-    }
 
-    return moves;
-  }
+      const attacks = this.findLegalAttacks(board);
+      moves.push(...attacks);
 
-  findLegalAttacks(board) {
-    const attacks = Array(
-      [this._x + this._vector, this._y + 1],
-      [this._x + this._vector, this._y - 1]
-    );
+      return moves;
+   }
 
-    attacks = this.filterOutBoardMoves(attacks);
-    return attacks;
-  }
+   findLegalAttacks(board) {
+      const attacks = [];
 
-  filterOutBoardMoves(possibleMoves) {
-    return possibleMoves.filter(el => {
-      return 0 <= el[0] && el[0] <= 7 && 0 <= el[1] && el[1] <= 7;
-    });
-  }
+      if (board[this._x + this._vector][this._y + 1]) {
+         attacks.push([this._x + this._vector, this._y + 1]);
+      }
+      if (board[this._x + this._vector][this._y - 1]) {
+         attacks.push([this._x + this._vector, this._y - 1]);
+      }
+
+      console.log(board[this._x + this._vector][this._y + 1]);
+
+      return attacks;
+   }
 }
 
 export default Pawn;
